a58ab4ff962ec55387a7c7a80641868c015973a6
msawo <msawo.inc@gmail.com>
2020-07-10 23:24:22 -0400

    Add TxList widget

--- lib/screen/homePage.dart
+++ lib/screen/homePage.dart
@@ -1,28 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-import '../model.dart/transaction.dart';
-import '../common.dart';
+import '../widgets/transactionList.dart';
+import '../models/transaction.dart';
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
-} 
+}
 
 class _HomePageState extends State<HomePage> {
-  List<Transaction> _transactionList = [
-    Transaction(
-      id: 't0',
-      title: 'Amzn MarketPlace',
-      amount: 1245.93,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't1',
-      title: 'Kayak Swiss Airlines',
-      amount: 845.33,
-      date: DateTime.now(),
-    ),
+  final List<Transaction> _userTx = [
+    // Transaction(
+    //   id: 't1',
+    //   title: 'Kayak Gambia Flight',
+    //   amount: 850.90,
+    //   date: DateTime.now(),
+    // ),
   ];
 
   @override
@@ -40,70 +34,7 @@ class _HomePageState extends State<HomePage> {
       ),
       body: SingleChildScrollView(
         child: SafeArea(
-          child: Column(
-            children: <Widget>[
-              Container(
-                height: 200,
-                color: Theme.of(context).accentColor,
-                width: double.infinity,
-                child: Card(
-                  child: Text('Chart'),
-                ),
-              ),
-              Card(
-                elevation: 3,
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).primaryColor,
-                        borderRadius: BorderRadius.circular(30.0),
-                        border: Border.all(
-                          color: Theme.of(context).primaryColorDark,
-                          width: 1.0,
-                        ),
-                      ),
-                      child: FittedBox(
-                        child: Text(
-                          '${_transactionList[0].amount.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            color: white,
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 8.0,
-                        vertical: 8.0,
-                      ),
-                      margin: EdgeInsets.symmetric(
-                          horizontal: 15.0, vertical: 14.0),
-                    ),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            '${_transactionList[0].title}',
-                            style: TextStyle(
-                              fontSize: 17.0,
-                              fontWeight: FontWeight.w500,
-                              height: 1.4,
-                            ),
-                          ),
-                          Text(
-                            DateFormat.yMMMMd("en_US").format(_transactionList[0].date),
-                            style: TextStyle(fontSize: 15, fontWeight: FontWeight.w300, fontStyle: FontStyle.italic, color: secondaryText),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
+          child: TransactionList(transactions: _userTx),
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
