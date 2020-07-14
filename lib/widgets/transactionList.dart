77003d819084579eb591804ec4f51e9707f6ad4e
msawo <msawo.inc@gmail.com>
2020-07-14 00:07:07 -0400

    Change ListView.builder to plain listview

--- lib/widgets/transactionList.dart
+++ lib/widgets/transactionList.dart
@@ -22,67 +22,67 @@ class TransactionList extends StatelessWidget {
               child: Text('Chart'),
             ),
           ),
-          ListView.builder(
-              itemCount: transactions.length,
-              itemBuilder: (ctx, index) {
-                return Card(
-                  elevation: 3,
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Theme.of(context).primaryColor,
-                          borderRadius: BorderRadius.circular(30.0),
-                          border: Border.all(
-                            color: Theme.of(context).primaryColorDark,
-                            width: 1.0,
+          ListView(
+            children: <Widget>[
+              Card(
+                elevation: 3,
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(30.0),
+                        border: Border.all(
+                          color: Theme.of(context).primaryColorDark,
+                          width: 1.0,
+                        ),
+                      ),
+                      child: FittedBox(
+                        child: Text(
+                          '\$ ${transactions[0].amount.toStringAsFixed(2)}',
+                          style: TextStyle(
+                            color: white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
-                        child: FittedBox(
-                          child: Text(
-                            '\$ ${transactions[index].amount.toStringAsFixed(2)}',
+                      ),
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 8.0,
+                        vertical: 8.0,
+                      ),
+                      margin: EdgeInsets.symmetric(
+                          horizontal: 15.0, vertical: 14.0),
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            '${transactions[0].title}',
                             style: TextStyle(
-                              color: white,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
+                              fontSize: 17.0,
+                              fontWeight: FontWeight.w500,
+                              height: 1.4,
                             ),
                           ),
-                        ),
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 8.0,
-                          vertical: 8.0,
-                        ),
-                        margin: EdgeInsets.symmetric(
-                            horizontal: 15.0, vertical: 14.0),
+                          Text(
+                            DateFormat.yMMMMd("en_US")
+                                .format(transactions[0].date),
+                            style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.w300,
+                                fontStyle: FontStyle.italic,
+                                color: secondaryText),
+                          ),
+                        ],
                       ),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              '${transactions[index].title}',
-                              style: TextStyle(
-                                fontSize: 17.0,
-                                fontWeight: FontWeight.w500,
-                                height: 1.4,
-                              ),
-                            ),
-                            Text(
-                              DateFormat.yMMMMd("en_US")
-                                  .format(transactions[index].date),
-                              style: TextStyle(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w300,
-                                  fontStyle: FontStyle.italic,
-                                  color: secondaryText),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              }),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
